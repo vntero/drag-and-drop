e0ec275744e176b80840a6505659dc1eb9fcefa0
vntero <shin.antero@gmail.com>
2022-04-20 18:10:26 +0200

    projects lists showing up as expected

--- src/app.ts
+++ src/app.ts
@@ -53,7 +53,19 @@ class ProjectList {
 
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = <HTMLElement>importedNode.firstElementChild
-        this.element.id = ''
+        this.element.id = `${this.type}-projects`
+        this.attach()
+        this.renderContent()
+    }
+
+    private renderContent() {
+        const listId = `${this.type}-projects-list`
+        this.element.querySelector('ul')!.id = listId
+        this.element.querySelector('h2')!.textContent = this.type.toUpperCase() + ' PROJECTS'
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('beforeend', this.element)
     }
 }
 
@@ -143,3 +155,5 @@ class ProjectInput {
 }
 
 const prjInput = new ProjectInput()
+const activePrjList = new ProjectList('active')
+const finishedPrjList = new ProjectList('finished')
