16575e3f42c7f3f5c0024a94198566091813f2ea
vntero <shin.antero@gmail.com>
2022-04-20 16:24:55 +0200

    adding a decorator for automatically bind the this keyword

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
+// autobind decorator
+function
+
+// project input class
 class ProjectInput {
     templateElement: HTMLTemplateElement
     hostElement: HTMLDivElement
     element: HTMLFormElement
+    titleInputElement: HTMLInputElement
+    descriptionInputElement: HTMLInputElement
+    peopleInputElement: HTMLInputElement
 
     constructor() {
         this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!
@@ -9,7 +16,23 @@ class ProjectInput {
 
         const importedNode = document.importNode(this.templateElement.content, true)
         this.element = <HTMLFormElement>importedNode.firstElementChild
+        this.element.id = 'user-input'
+
+        this.titleInputElement = <HTMLInputElement>this.element.querySelector('#title')
+        this.descriptionInputElement = <HTMLInputElement>this.element.querySelector('#description')
+        this.peopleInputElement = <HTMLInputElement>this.element.querySelector('#people')
+
+        this.configure()
         this.attach()
+    }
+
+    private submitHandler(event: Event) {
+        event.preventDefault()
+        console.log(this.titleInputElement.value)
+    }
+
+    private configure() {
+        this.element.addEventListener('submit', this.submitHandler.bind(this))
     }
 
     private attach() {
