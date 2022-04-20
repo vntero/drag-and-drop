c1eb73a23d19a89b807ed55823cd08b2dbd45a71
vntero <shin.antero@gmail.com>
2022-04-20 16:42:01 +0200

    created a private method to grab user input

--- src/app.ts
+++ src/app.ts
@@ -36,10 +36,25 @@ class ProjectInput {
         this.attach()
     }
 
+    private gatherUserInput(): [string, string, number] | void {
+        const enteredTitle = this.titleInputElement.value
+        const enteredDescription = this.descriptionInputElement.value
+        const enteredPeople = this.peopleInputElement.value
+
+        //not scalable enough of a solution
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredPeople.trim().length === 0) {
+            alert('Invalid input, please try again')
+            return
+        } else {
+            return [enteredTitle, enteredDescription, +enteredPeople]
+        }
+    }
+
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault()
-        console.log(this.titleInputElement.value)
+        const userInput = this.gatherUserInput()
+        // console.log(this.titleInputElement.value)
     }
 
     private configure() {
