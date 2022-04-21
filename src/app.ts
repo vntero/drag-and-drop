96fa3cb7c7a0b531ca82bbf9b07faf2d8bac6820
vntero <shin.antero@gmail.com>
2022-04-21 17:09:04 +0200

    dragging working as expected, grabbed target triggers a visual effect on the client side

--- src/app.ts
+++ src/app.ts
@@ -127,7 +127,7 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
 }
 
 //project item class
-class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable{
     private project: Project
 
     get persons() {
@@ -146,7 +146,19 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
         this.renderContent()
     }
 
-    configure() {}
+    @autobind
+    dragStartHandler(event: DragEvent) {
+        console.log(event)
+    }
+
+    dragEndHandler(_: DragEvent) {
+        console.log('DragEnd')
+    }
+
+    configure() {
+        this.element.addEventListener('dragstart', this.dragStartHandler)
+        this.element.addEventListener('dragend', this.dragEndHandler)
+    }
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title
