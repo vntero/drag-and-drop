bc52a096ed7c1a24f7f9e9ab6fb3bfb1e035ce80
vntero <shin.antero@gmail.com>
2022-04-21 17:03:51 +0200

    added 2 interfaces for handling the dragging and dropping events

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+// drag and drop interfaces
+interface Draggable {
+    dragStartHandler(event: DragEvent): void
+    dragEndHandler(event: DragEvent): void
+}
+
+interface DragTarget {
+    dragOverHandler(event: DragEvent): void
+    dropHandler(event: DragEvent): void
+    dragLeaveHandler(event: DragEvent): void
+}
+
 // project type
 enum ProjectStatus { Active, Finished}
 
