3ddf50c7d0904955b534d8ce5735dd5a720c4e46
vntero <shin.antero@gmail.com>
2022-04-21 10:20:39 +0200

    starting with Interfaces to implement drag and drop functionality

--- src/app.ts
+++ src/app.ts
@@ -138,7 +138,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
 
     renderContent() {
         this.element.querySelector('h2')!.textContent = this.project.title
-        this.element.querySelector('h3')!.textContent = this.project.people.toString() + ' assigned'
+        this.element.querySelector('h3')!.textContent = this.persons + ' assigned'
         this.element.querySelector('p')!.textContent = this.project.description
     }
 }
